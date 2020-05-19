a58610d4d0c1902fa29e6537dd9822377176c6f4
Robert <robert.a.dallyn@kcl.ac.uk>
2020-05-19 16:34:57 +0100

    initial commit

---
+++ utils/ICC_functions.py
@@ -0,0 +1,212 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import numpy as np
+import pandas as pd
+
+
+def mask_data(data):
+    
+    ####Pass data as a list
+    tmp = np.stack(data, axis=2)
+    mask = np.any(np.isnan(tmp), axis=(1,2))
+    return mask
+
+def Add_To_Dataframe(Data, Paradigms, Metrics, ROIS, df, GDT=True):
+    X = Data.shape
+    if GDT==True:
+        GDT_stat=1
+    else:
+        GDT_stat=0       
+    for Paradigm_index, Paradigm in enumerate(Paradigms):
+        for Metric_index, Metric in enumerate(Metrics):
+            for ROI_index, ROI in enumerate(ROIS):
+                ndf = pd.DataFrame( { "Paradigm" : Paradigms[Paradigm_index], "Metric" : Metrics[Metric_index], "ROI" : ROIS[ROI_index], 
+                           "Value" : Data[ROI_index, Metric_index, Paradigm_index], "GDT" : GDT_stat }, index = [0])
+                df=pd.concat([df,ndf])
+        
+    df=df.set_index(np.arange(df.shape[0]))
+    return df
+
+
+def load_images(Reg_files, GDT_files):
+    #Initialize 5-D array
+    reg_concat_image = np.zeros((256,256,150, num_subjects, num_sessions))
+    GDT_concat_image = np.zeros((256,256,150, num_subjects, num_sessions))
+    for i in range(0,num_subjects):
+        for j in range(0,num_sessions):
+
+            print("subject: ", i+1)
+            print("session: ", j+1)
+
+            if os.path.isfile(Regular_files_array[i,j]):
+                reg_concat_image[:,:,:,i,j]=nipy.load_image(Regular_files_array[i,j]).get_data()
+            else:
+                print("no File")
+                reg_concat_image[:,:,:,i,j]=np.nan
+
+            if os.path.isfile(GDT_files_array[i,j]):
+                GDT_concat_image[:,:,:,i,j]=nipy.load_image(GDT_files_array[i,j]).get_data()
+            else:
+                print("no File")
+                GDT_concat_image[:,:,:,i,j]=np.nan 
+                
+    return (reg_concat_image, GDT_concat_image)
+
+
+
+def ICC_Voxelwise_Vector(data,mask):
+    
+    """Pass mask as a boolean np array
+    """
+    
+    ##########################################################
+    #Need to add in to drop image if any session is all nans!#
+    ##########################################################
+    
+    # 30*3 list of images which are all NaNs
+    Image_Nans = np.all(np.isnan(data[mask]), axis=0)
+    
+    # 30*1 list of subjects for which any session is all NaNs
+    Session_Nans = Image_Nans.any(axis=1)
+    
+    # Mask data to remove rows with NaN sessions
+    data = data[:,:,:,~Session_Nans,:]
+    
+    #Drop negatives
+    data=np.absolute(data)
+    
+    ##Shape of input data
+    datshape = data.shape
+    
+
+    ##########################################################
+    # ICC CALCULATION
+    ##########################################################
+    
+    grandmeans = np.zeros([datshape[0],datshape[1],datshape[2]])
+    Subject_Means = np.zeros([datshape[0],datshape[1], datshape[2], datshape[3]])
+    Session_Means = np.zeros([datshape[0],datshape[1], datshape[2], datshape[4]])
+
+    grandmeans[mask]=np.mean(data[mask])
+
+    Subject_Means[mask] = np.mean(data[mask], axis=2)
+    Session_Means[mask] = np.mean(data[mask], axis=1)
+    grandmeans[mask] = np.mean(data[mask],axis=(1,2))
+
+
+    #INIT vars
+
+    BMS = np.zeros([datshape[0],datshape[1],datshape[2]])
+    WMS = np.zeros([datshape[0],datshape[1],datshape[2]])
+    JMS = np.zeros([datshape[0],datshape[1],datshape[2]])
+    EMS = np.zeros([datshape[0],datshape[1],datshape[2]])
+    SST = np.zeros([datshape[0],datshape[1],datshape[2]])
+    PMS = np.zeros([datshape[0],datshape[1],datshape[2]])
+    sv=np.zeros(datshape[1])
+
+
+    ##Loops over Subject
+    BMS[mask] = ((Subject_Means[mask] - grandmeans[mask,None])**2).sum(axis=1)
+    
+    #Loops over Subject then Session
+    WMS[mask] = ((data[mask] - Subject_Means[mask,:,None])**2).sum(axis=(1,2))
+    EMS[mask] = ((data[mask] - Subject_Means[mask,:,None] - Session_Means[mask,None,:] + grandmeans[mask,None,None])**2).sum(axis=(1,2))
+    SST[mask] = ((data[mask] - grandmeans[mask,None,None])**2).sum(axis=(1,2))
+    
+    #Loops over Session
+    JMS[mask] = ((Session_Means[mask] - grandmeans[mask,None])**2).sum(axis=1)
+    
+    #Loops over Session then Subject
+    PMS[mask] = ((data[mask] - Session_Means[mask,None,:])**2).sum(axis=(1,2))
+
+            #        sv[j] = sv[j] + (data[i,j] - Session_Means[j])**2          
+
+    nsubjects=datshape[3]
+    nsessions=datshape[4]
+
+    #Correct
+
+    BMS[mask] = nsessions*BMS[mask]/(nsubjects-1);
+    WMS[mask] = WMS[mask]/(nsessions-1)/nsubjects;
+    JMS[mask] = nsubjects*JMS[mask]/(nsessions-1);
+    EMS[mask] = EMS[mask]/(nsessions-1)/(nsubjects-1); 
+    PMS[mask] = PMS[mask]/(nsessions-1)/(nsubjects-1); 
+    #Here we are dividing by zero
+    ICC = (BMS - EMS) / (BMS + (nsessions-1)*EMS)
+    CVws = (WMS ** 0.5) / grandmeans;
+    CVbs = (BMS ** 0.5) / grandmeans;
+    #Here we are dividing by zero
+    print(np.any(np.isnan(BMS)))
+    print(np.any(np.nonzero(BMS)))
+
+
+    #return( BMS, WMS, JMS, EMS, PMS, Subject_Means, Session_Means)
+    return (ICC, CVws, CVbs)   
+
+def ICC(data, mask=None):
+    
+    ####Make mask if none passed
+    if mask is None:
+        mask = np.any(np.isnan(data), axis=1)
+
+    ####Drop rows if any nans
+    if np.any(mask) == True:
+        data = data[~mask]
+    
+    #Drop negatives
+    data=np.absolute(data)
+    
+    ICC = 0
+    CVws = 0
+    CVbs = 0
+    
+    #Calculating various means
+    grandmean = np.mean(data)
+    Session_Means=np.mean(data, axis=0)
+    Subject_Means=np.mean(data, axis=1)
+
+    #INIT vars
+    BMS=0; WMS=0; JMS=0; EMS=0;
+    SST=0; PMS=0
+    sv=np.zeros(data.shape[1])
+
+    nsubjects=data.shape[0]
+    nsessions=data.shape[1]
+
+    
+    #Calculate Mean Squares
+    for i in range (0, data.shape[0]):
+        BMS = BMS + (Subject_Means[i]-grandmean)**2
+
+        for j in range (0, data.shape[1]):
+            WMS = WMS + (data[i,j]-Subject_Means[i])**2
+
+            EMS = EMS + (data[i,j] - Subject_Means[i] - Session_Means[j] + grandmean)**2
+            SST = SST + (data[i,j] - grandmean)**2
+
+    for j in range(0,data.shape[1]):
+        JMS = JMS + (Session_Means[j] - grandmean)**2
+
+
+    for j in range(0,data.shape[1]):
+        for i in range(0,data.shape[0]):
+            PMS = PMS + (data[i,j] - Session_Means[j])**2
+            sv[j] = sv[j] + (data[i,j] - Session_Means[j])**2            
+
+    #Correct
+    BMS = nsessions*BMS/(nsubjects-1);
+    WMS = WMS/(nsessions-1)/nsubjects;
+    JMS = nsubjects*JMS/(nsessions-1);
+    EMS = EMS/(nsessions-1)/(nsubjects-1); 
+    PMS = PMS/(nsessions-1)/(nsubjects-1); 
+
+    ICC = (BMS - EMS) / (BMS + (nsessions-1)*EMS)
+    CVws = (WMS ** 0.5) / grandmean;
+    CVbs = (BMS ** 0.5) / grandmean;
+    
+    return (ICC, CVws, CVbs)
+
---
+++ utils/Variable_setup.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+import os
+from configparser import ConfigParser
+parser = ConfigParser()
+
+parser.read('./../config.ini')
+
+Base_path = parser.get('base_vars','Base_Path')
+ROIs_path = parser.get('base_vars','ROIs_path')
+Caudate_ROIs_path = parser.get('base_vars','Caudate_ROIs_path')
+data_path = parser.get('base_vars','data_path')
+num_subjects = float(parser.get('base_vars','num_subjects'))
+num_sessions = float(parser.get('base_vars','num_sessions'))
+
+
+#Generate list of all the ROIs
+ROIS = [f for f in os.listdir(path=ROIs_path) if os.path.isfile(os.path.join(ROIs_path,f))]
+
+ROIS = list(filter(lambda k: 'DS' not in k, ROIS))
+ROIS = list(filter(lambda k: '.afp' not in k, ROIS))
+ROIS = list(filter(lambda k: '.csv' not in k, ROIS))
+
+#Generate list of all the ROIs
+Caudate_ROIS = [f for f in os.listdir(path=Caudate_ROIs_path) if os.path.isfile(os.path.join(Caudate_ROIs_path,f))]
+
+Caudate_ROIS = list(filter(lambda k: 'DS' not in k, ROIS))
+Caudate_ROIS = list(filter(lambda k: '.afp' not in k, ROIS))
+Caudate_ROIS = list(filter(lambda k: '.csv' not in k, ROIS))
+
+
+# In[ ]:
+
+
+Metrics = [ "Axial" , "Fractional_Anisotropy" , "Radial" , "Trace" ]
+Extracted_Metrics = [ "Axial" , "Fractional_Anisotropy" , "Radial" , "Trace" ]
+Native_Metrics = [ "Axial" , "Fractional_Anisotropy" , "Radial" , "Trace" ]
+B0_Removed_Metrics = [ "Axial" , "Fractional_Anisotropy" , "Radial" , "Trace" ]
+Jacobians = [ "Jacobians" ]
+Extracted_Jacobians = [ "Ext_Jacobians" ]
+Advanced_Metrics = [ "DKI" , "DKI_Thresholded" , "NODDI" , "MAPMRI" ]
+DKI_Metrics = [ "Radial_Kurtosis" , "Axial_Kurtosis" , "Mean_Kurtosis" , "Kurtosis_Anisotropy" ]
+DKI_Thresh_Metrics = [ "Radial_Kurtosis_Thresh" , "Axial_Kurtosis_Thresh" , "Mean_Kurtosis_Thresh", "Kurtosis_Anisotropy_Thresh" ]
+MAPMRI_Metrics = [ "Return_To_Plane_Probability" , "Return_To_Origin_Probability" , "Return_To_Axis_Probability" 
+                  , "Non_Gaussianity" , "PAth", "Propagator_Anisotropy" , "NGpar" , "NGperp" ]
+NODDI_Metrics = [ "Orientation_Dispersion" , "Isotropic_Volume_Fraction" , "IntraCellular_Volume_Fraction" ]
+
+
+Basic_Metrics_dict = { 'Axial' : 'AD' , "Fractional_Anisotropy" : 'FA' , "Jacobians" : 'JAC' , "Radial" : 'RD' , "Trace" : 'TR' }
+Extracted_Metrics_dict = { 'Ext_Axial' : 'AD_ext' , "Ext_Fractional_Anisotropy" : 'FA_ext' , "Ext_Radial" : 'RD_ext' , "Ext_Trace" : 'TR_ext' }
+Native_Metrics_dict = { 'Native_Axial' : 'AD_N' , "Native_Fractional_Anisotropy" : 'FA_N' , "Native_Radial" : 'RD_N' , "Native_Trace" : 'TR_N' }
+B0_Removed_Metrics_dict = { "B0_Axial" : "AD_B" , "B0_Fractional_Anisotropy" : "FA_B" , "B0_Radial" : "RD_B" , "B0_Trace" : "TR_B" }
+Jacobians_Metrics_dict = { "Jacobians" : "JAC" }
+Extracted_Jacobians_Metrics_dict = { "Ext_Jacobians" : "JAC_ext" }
+DKI_dict = { 'Radial_Kurtosis' : 'RK' , "Axial_Kurtosis" : 'AK' , "Mean_Kurtosis" : 'MK' , "Kurtosis_Anisotropy" : 'KA' }
+DKI_Thresh_dict = { 'Radial_Kurtosis_Thresh' : 'RK_T' , "Axial_Kurtosis_Thresh" : 'AK_T' , "Mean_Kurtosis_Thresh" : 'MK_T' , "Kurtosis_Anisotropy_Thresh" : "KA_T" }
+MAPMRI_dict = { "Return_To_Plane_Probability" : 'RTPP' , "Return_To_Origin_Probability" : 'RTOP', "Return_To_Axis_Probability" : 'RTAP' ,
+                  "Non_Gaussianity" : 'NG' , "PAth" : 'PAth' , "Propagator_Anisotropy" : 'PA' , "NGpar" : 'NGpar' , "NGperp" : 'NGperp' }
+NODDI_dict = { "Orientation_Dispersion" : 'OD' , "Isotropic_Volume_Fraction" : 'ISOVF' , "IntraCellular_Volume_Fraction" : 'ICVF' }
+Metrics_dict = {'Metrics' : Metrics, 'Ext_Metrics' : Extracted_Metrics, 'Native' : Native_Metrics, 'B0_Removed' : B0_Removed_Metrics,
+             'Jacobians' : Jacobians, 'Ext_Jacobians' : Extracted_Jacobians, 'Advanced' : Advanced_Metrics, 'DKI': DKI_Metrics,
+             'DKI_Thresh' : DKI_Thresh_Metrics, 'MAPMRI' : MAPMRI_Metrics, 'NODDI' : NODDI_Metrics}
+Label_Metrics_Dict = {**Label_Metrics_dict, **Extracted_Metrics_dict, **Native_Metrics_dict, **B0_Removed_Metrics_dict, **Jacobians_Metrics_dict,
+                     **DKI_dict, **DKI_Thresh_dict, **MAPMRI_dict, **NODDI_dict}
+Advanced_Metrics_Dict = {**DKI_Thresh_dict, **MAPMRI_dict, **NODDI_dict}
+
+Metrics_paradigms = [ "Single_Band" , "Multi_Band" , "Multi_Shell" ]
+Extracted_Metrics_paradigms = [ "Single_Shell_Extracted" ]
+Native_Metrics_paradigms = [ "Native_Single_Band" , "Native_Multi_Band" , "Native_Multi_Shell" ]
+B0_Removed_Metrics_paradigms = [ "Single_Band_B0_Removed" ]
+Jacobians_paradigms = [ "Single_Band" , "Multi_Band" , "Multi_Shell" ]
+Advanced_Metrics_paradigms = [ "Multi_Shell" ]
+DKI_Metrics_paradigms = [ "Multi_Shell" ]
+DKI_Thresh_Metrics_paradigms = [ "Multi_Shell" ]
+MAPMRI_Metrics_paradigms = [ "Multi_Shell" ]
+NODDI_Metrics_paradigms = [ "Multi_Shell" ]
+
+Paradigms_dict = {'Metrics' : Metrics_paradigms, 'Ext_Metrics' : Extracted_Metrics_paradigms, 'Native' : Native_Metrics_paradigms,
+                  'B0_Removed' : B0_Removed_Metrics_paradigms, 'Jacobians' : Jacobians_paradigms, 'Advanced' : Advanced_Metrics_paradigms, 'DKI' : DKI_Metrics_paradigms, 'DKI_Thresh' : DKI_Thresh_Metrics_paradigms,
+                 'MAPMRI' : MAPMRI_Metrics_paradigms, 'NODDI' : NODDI_Metrics_paradigms}
+
